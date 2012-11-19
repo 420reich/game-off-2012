419c0e33d4b121083b1387aec6871afb59a2d3ed
Guilherme Souza <guivideojob@gmail.com>
2012-11-19 18:57:25 -0200

    Robot new fields

---
+++ fightcode/migrations/20121119184258-change_robot_table.js
@@ -0,0 +1,21 @@
+var newFields = ['victories', 'defeats', 'draws'];
+
+module.exports = {
+  up: function(migration, DataTypes) {
+    for (var i=0; i < newFields.length; i++) {
+      migration.addColumn(
+        'Robots',
+        newFields[i],
+      {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        default: 0
+      });
+    }
+  },
+  down: function(migration) {
+    for (var i=0; i < newFields.length; i++) {
+      migration.removeColumn('Robots', newFields[i]);
+    }
+  }
+};
--- fightcode/models/robot.js
+++ fightcode/models/robot.js
@@ -2,8 +2,40 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('Robot', {
         gists: { type: DataTypes.STRING, allowNull: false},
-        ownerLogin: { type: DataTypes.STRING, allowNull: false}
-    }, {
+        ownerLogin: { type: DataTypes.STRING, allowNull: false},
+        victories: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0},
+        defeats: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0},
+        draws: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0},
+        score: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0}
+    },{
+        instanceMethods:{
+            updateScore: function(){
+                var WonPoints = this.victories * 3,
+                    LostPoints = this.defeats * -1,
+                    DrawPoints = this.draws;
+
+                this.score = this.score + WonPoints + LostPoints + DrawPoints;
+                this.save();
+            },
+
+            addVictory: function() {
+                this.victories += 1;
+                this.updateScore();
+            },
+
+            addDefeat: function() {
+                this.defeats += 1;
+                this.updateScore();
+            },
+
+            addDraw: function() {
+                this.draws += 1;
+                this.updateScore();
+            }
+        }
+      },
+      {
         underscored: true
-    });
+      }
+    );
 };
