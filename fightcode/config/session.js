e6fc7846dddfd1aa64e40777a918fe202e6a0694
Guilherme Souza <guivideojob@gmail.com>
2012-11-28 10:39:37 -0200

    expiring session

--- fightcode/config/session.js
+++ fightcode/config/session.js
@@ -3,4 +3,6 @@ var express = require('express'),
     C3Store = require('c3store')(express);
 
 module.exports = {secret: '78c6c98f-17fe-46e9-9ada-25f3464d7a6d',
+                  maxAge : new Date(Date.now() + 60000 * 20),
+                  expires : new Date(Date.now() + 60000 * 20),
                   store: new C3Store(sequelize)};
